2e681329476edee43d79e8f08b973fd4dc8aa488
charlesalexisduguay04 <charles-alexis.duguay@umontreal.ca>
2025-09-05 10:35:57 -0400

    debug

--- src/LandscapeMetrics.jl
+++ src/LandscapeMetrics.jl
@@ -51,12 +51,6 @@ export largestpatchindex
 include("area_and_edge/radiusofgyration.jl")
 export radiusofgyration
 
-include("area_and_edge/totaledge.jl")
-export totaledge
-
-include("area_and_edge/edgedensity.jl")
-export edgedensity
-
 # Shape
 include("shape/paratio.jl")
 export paratio, perimeterarearatio
