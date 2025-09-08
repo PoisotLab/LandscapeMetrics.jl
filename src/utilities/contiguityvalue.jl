9007cb34ee7b9386a3fe1ee4df80d7ec05a34e5c
charlesalexisduguay04 <charles-alexis.duguay@umontreal.ca>
2025-09-08 12:31:57 -0400

    contiguityvalue

--- src/utilities/contiguityvalue.jl
+++ src/utilities/contiguityvalue.jl
@@ -1,19 +1,20 @@
-template = [
+B = [
     1 2 1;
     2 1 2;
     1 2 1
 ]
 
-function contiguityvalue(l::Landscape, template)
-    nrows, ncols = size(template)
+function contiguityvalue(A::AbstractMatrix, B::AbstractMatrix)
+    nrows, ncols = size(B)
+    T = eltype(A)
     result = zeros(T, nrows, ncols)
     # Pad the matrix with zeros on all sides
     padded = zeros(T, nrows+2, ncols+2)
-    padded[2:end-1, 2:end-1] .= matrix
+    padded[2:end-1, 2:end-1] .= A
     for i in 1:nrows
         for j in 1:ncols
             neighborhood = padded[i:i+2, j:j+2]
-            result[i, j] = sum(neighborhood .* template)
+            result[i, j] = sum(neighborhood .* B)
         end
     end
     return result
@@ -24,9 +25,14 @@ end
         1 1;
         1 1
     ]
+    B = [
+    1 2 1;
+    2 1 2;
+    1 2 1
+]
     expected = [
         6 6;
         6 6
     ]
-    @test contiguityvalue(A, template) == expected
+    @test contiguityvalue(A, B) == expected
 end
