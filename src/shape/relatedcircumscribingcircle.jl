d1d41570bcc3fa5bc499da34ee1e888b0e5cc634
charlesalexisduguay04 <charles-alexis.duguay@umontreal.ca>
2025-09-08 14:22:12 -0400

    finally

--- src/shape/relatedcircumscribingcircle.jl
+++ src/shape/relatedcircumscribingcircle.jl
@@ -1,13 +1,20 @@
-function related_circumscribing_circle(patch_points::Vector{Tuple{Float64, Float64}}, patch_area::Float64)
-    circle = welzl_algorithm(copy(patch_points), [])
+function related_circumscribing_circle(l::Landscape, patch)
+    patch_area = area(l, patch)
+    centers = cellcenters(l)
+    patch_cells = findall(patches(l) .== patch)
+    patch_points = [centers[I] for I in patch_cells]
+    circle = minimum_enclosing_circle(copy(patch_points))
     circle_area = π * circle.radius^2
+    println("Patch area: ", patch_area)
+    println("Circle area: ", circle_area)
     return 1 - (patch_area / circle_area)
 end
 
-
-@testitem "related_circumscribing_circle works on a triangle" begin
-    points = [(0.0, 0.0), (1.0, 0.0), (0.0, 1.0)]
-    area = 0.5
-    rcc = related_circumscribing_circle(points, area)
-    @test isapprox(rcc, 1 - (area / (π * (sqrt(2)/2)^2)), atol=1e-6)
+@testitem "We can measure the related circumscribing circle for a patch" begin
+    A = [
+         1 1 1
+    ]
+    L = Landscape(A)
+    P = patches(L)
+    @test related_circumscribing_circle(L, 1) ≈ 0.04507034144
 end
