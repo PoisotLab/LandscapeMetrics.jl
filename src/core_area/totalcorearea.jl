a3b00b0fea2a305f15a8bb1258372798abf0496b
charlesalexisduguay04 <charles-alexis.duguay@umontreal.ca>
2025-11-13 12:06:37 -0500

    clear

--- src/core_area/totalcorearea.jl
+++ src/core_area/totalcorearea.jl
@@ -1,12 +1,23 @@
+"""
+    total_core_area(l::Landscape, depth)
 
+Total core area of all patches in the landscape at a specified depth.
+"""
 function total_core_area(l::Landscape, depth)
+
+    # Getting all unique patch IDs
     patch_ids = unique(patches(l))
+
+    # Initializing total core area
     total_core = 0.0
+
+    # Summing core areas of all patches
     for pid in patch_ids
         if pid != 0
             total_core += core_area(l, pid, depth)
         end
     end
+    
     return total_core
 end
 
