3904552687e7bb048d26376d247b0356a4b45de0
charlesalexisduguay04 <charles-alexis.duguay@umontreal.ca>
2025-11-13 13:16:41 -0500

    fix

--- src/core_area/numberofdisjunctcorearea.jl
+++ src/core_area/numberofdisjunctcorearea.jl
@@ -1,30 +1,31 @@
+"""
+
+    number_of_disjunct_core_areas(l::Landscape, class_id, depth)
+
+Count the number of disjunct core areas for all patches of a given class in the landscape,
+
+"""
 function number_of_disjunct_core_areas(l::Landscape, class_id, depth)
     p = patches(l)
 
-    # build patch id -> class map
-    patch_to_class = Dict{Any, Any}()
-    for idx in CartesianIndices(p)
-        pid = p[idx]
-        if !haskey(patch_to_class, pid)
-            patch_to_class[pid] = pid == 0 ? 0 : l[idx]
+    patch_ids = unique(patches(l))
+    nb_core = 0
+
+    for pid in patch_ids
+        if pid == 0
+            continue
         end
+        loc = findfirst(isequal(pid), p)
+
+        if l[loc] != class_id
+            continue
+        end
+
+    ncore = count_core_areas(l, pid, depth)
+        nb_core += ncore
     end
 
-    count = 0
-    for (pid, cls) in patch_to_class
-        if pid != 0 && cls == class_id
-            # build integer mask for this patch (1 for cells in patch, 0 otherwise)
-            A = zeros(Int, size(p))
-            for idx in findall(isequal(pid), p)
-                A[idx] = 1
-            end
-            core_mask = compute_core_mask(A, depth)
-            # count connected core components
-            ncores = count_core_areas(core_mask)
-            count += ncores
-        end
-    end
-    return count
+    return nb_core
 end
 
 
@@ -44,4 +45,48 @@ end
     patches!(L)
     nodca = number_of_disjunct_core_areas(L, 1, 1)
     @test nodca == 2
+end
+
+
+"""
+
+    number_of_disjunct_core_areas(l::Landscape, depth)
+
+    Compute the number of disjunct core areas for all patches in the landscape.
+"""
+
+function number_of_disjunct_core_areas(l::Landscape, depth)
+    p = patches(l)
+
+    patch_ids = unique(patches(l))
+    nb_core = 0
+
+    for pid in patch_ids
+        if pid == 0
+            continue
+        end
+
+        ncore = count_core_areas(l, pid, depth)
+        nb_core += ncore
+    end
+
+    return nb_core
+end
+
+@testitem "We can compute the number of disjunct core areas for a patch" begin
+    A = [
+        0 1 1 1 1 1 0;
+        0 1 1 1 1 1 0;
+        0 1 1 1 1 1 0;
+        0 0 0 0 0 0 0;
+        0 0 0 0 1 1 1;
+        0 0 0 0 1 1 1;
+        1 1 1 0 2 2 2;
+        1 1 1 0 2 2 2;
+        1 1 1 0 2 2 2
+    ]
+    L = Landscape(A)
+    patches!(L)
+    nodca = number_of_disjunct_core_areas(L, 1)
+    @test nodca == 4
 end
