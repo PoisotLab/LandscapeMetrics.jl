2150b261c381fe9be6251e31234903308381e66a
charlesalexisduguay04 <charles-alexis.duguay@umontreal.ca>
2025-11-13 19:26:53 -0500

    merging fixes

--- src/LandscapeMetrics.jl
+++ src/LandscapeMetrics.jl
@@ -68,12 +68,8 @@ export total_core_area
 include("core_area/Ncore.jl")
 export count_core_areas
 
-
 include("core_area/coreareapercentage.jl")
 export core_area_percentage
-
-include("core_area/numberofdisjunctcoreareas.jl")
-export number_of_disjunct_core_areas
 
 include("core_area/numberofdisjunctcorearea.jl")
 export number_of_disjunct_core_areas
