52efa90d4b2329811188b16673986ab8df4056cc
charlesalexisduguay04 <charles-alexis.duguay@umontreal.ca>
2026-01-26 13:34:06 -0500

    prd fix

--- src/diversity/patchrichnessdensity.jl
+++ src/diversity/patchrichnessdensity.jl
@@ -9,3 +9,14 @@ function patchrichnessdensity(l::Landscape)
     area = totalarea(l)
     return pr / area
 end
+
+@testitem "We can measure patch richness density" begin
+    A = [
+        1 1 1 2 1 2;
+        1 2 1 2 1 2;
+        1 1 1 2 1 2]
+    L = Landscape(A)
+
+    prd = patchrichnessdensity(L)
+    @test isapprox(prd, 2 / 18)
+end
