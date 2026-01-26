ac156ba62777edd5fccd6918961d227140e72e00
charlesalexisduguay04 <charles-alexis.duguay@umontreal.ca>
2026-01-26 09:18:43 -0500

    edge density fixes

--- src/area_and_edge/edgedensity.jl
+++ src/area_and_edge/edgedensity.jl
@@ -1,23 +1,23 @@
 """
-   edgedensity(l::Landscape)
+   edgedensity(l::Landscape, class_val::Int)
 
-Returns the edge density of each class in the landscape, defined as the total edge length of a class divided by the total area.
+Returns the edge density of a specific class in the landscape, defined as the total edge length of a class divided by the total area.
 
 """
 
-function edgedensity(l::Landscape, class_val)
+function edgedensity(l::Landscape, class_val::Int)
     return totaledge(l, class_val) / totalarea(l)
 end
 
 
-@testitem "We can calculate the edge density of a landscape" begin
+@testitem "We can calculate the edge density for a specific class" begin
     A = [
-        1 1 1 2 2 2;
-        1 1 1 2 2 2;
-        2 2 2 2 2 2
+        1 1 1 2 1 2;
+        1 2 1 2 1 2;
+        1 1 1 2 1 2
     ]
-    L = Landscape(A)
-    @test edgedensity(L) == 5/18
+    L = Landscape(A, )
+    @test edgedensity(L, 1) == 13/18
 end
 
 """
@@ -26,4 +26,14 @@ Returns the edge density of the landscape.
 """
 function edgedensity(l::Landscape)
     return totaledge(l) / totalarea(l)
+end
+
+@testitem "We can calculate the edge density of a landscape" begin
+    A = [
+        1 1 1 2 1 2;
+        1 2 1 2 1 2;
+        1 1 1 2 1 2
+    ]
+    L = Landscape(A, )
+    @test edgedensity(L) == 13/18
 end
